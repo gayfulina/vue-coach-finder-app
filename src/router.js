bf4a90d806fc74d626cf7d0a837039c2627ec9ba
Irina Gayfulina <irinag@shapeways.com>
2022-03-20 16:54:05 -0700

    auth

--- src/router.js
+++ src/router.js
@@ -5,22 +5,24 @@ import CoachRegistration from '@/pages/coaches/CoachRegistration';
 import ContactCoach from '@/pages/request/ContactCoach';
 import RequestReceived from '@/pages/request/RequestReceived';
 import NotFound from '@/pages/NotFound';
+import UserAuth from '@/pages/auth/UserAuth';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/', redirect: 'coaches'},
+    { path: '/', redirect: 'coaches' },
     { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
       component: CoachDetail,
       props: true,
       children: [
-        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+        { path: 'contact', component: ContactCoach } // /coaches/c1/contact
       ]
     },
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestReceived },
+    { path: '/auth', component: UserAuth },
     { path: '/:notFound(.*)*', component: NotFound }
   ]
 });
